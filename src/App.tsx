0daba6b4b725a0234717cfe7537565594ce8096d
Bjarte Stien Karlsen <bjarte@bjartek.org>
2024-12-19 14:01:38 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="App">
+      <button className="btn btn-primary">Hello, Vite + React + DaisyUI!</button>
+    </div>
+  )
+}
+
+export default App
